5d03798636a5c9f67e9cad2d97c0f3b0bd11f69e
AidanJP <aidanpendlebury@gmail.com>
2021-02-07 17:43:57 +0000

    Refactor so program is only running if past sunset.

--- main.py
+++ main.py
@@ -7,67 +7,67 @@ import imutils
 import cv2
 from sunset import past_sunset
 
-# Initialise light manager
-switches = SwitchManager()
-device = switches.find_devices()[0].get_alias()
+
 light_on = False
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-a", "--min-area", type=int, default=1, help="minimum area size")
-args = vars(ap.parse_args())
+while not light_on:
 
-# Start capturing video from the webcam and give enough time to get out of the room so that the base
-# picture is of an empty room.
-vs = VideoStream(src=0).start()
-time.sleep(10.0)
-print("We'll get the first frame now")
+    # Only start recording if it's already past sunset
+    if past_sunset():
 
-# initialize the first frame in the video stream
-firstFrame = None
+        # Initialise light manager
+        switches = SwitchManager()
+        device = switches.find_devices()[0].get_alias()
 
-# loop over the frames of the video
-while True:
-    # grab the current frame
-    frame = vs.read()
+        # construct the argument parser and parse the arguments
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-a", "--min-area", type=int, default=1, help="minimum area size")
+        args = vars(ap.parse_args())
 
-    # if the frame could not be grabbed, something's not right.
-    if frame is None:
-        break
+        # Start capturing video from the webcam and give enough time to get out of the room so that the base
+        # picture is of an empty room.
+        vs = VideoStream(src=0).start()
+        time.sleep(10.0)
 
-    # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=500)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        # initialize the first frame in the video stream
+        firstFrame = None
 
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        firstFrame = gray
-        continue
+        # loop over the frames of the video
+        while not light_on:
+            # grab the current frame
+            frame = vs.read()
 
-    # compute the absolute difference between the current frame and first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+            # if the frame could not be grabbed, something's not right.
+            if frame is None:
+                break
 
-    # dilate the thresholded image to fill in holes, then find contours on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(contours)
+            # resize the frame, convert it to grayscale, and blur it
+            frame = imutils.resize(frame, width=500)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    # loop over the contours
-    for contour in contours:
-        # if the contour is too small, ignore it
-        if cv2.contourArea(contour) < args["min_area"]:
-            print("Contour not significant enough.")
-            continue
+            # if the first frame is None, initialize it
+            if firstFrame is None:
+                print(f"{datetime.datetime.now()}: Storing reference frame.")
+                firstFrame = gray
+                continue
 
-        if past_sunset():
-            print("It's past sunset")
-        else:
-            print("We have detected motion but it's not past sunset??")
-        if not light_on and past_sunset():
-            print(f"{datetime.datetime.now()}: Motion detected")
-            switches.switch_on(device)
-            light_on = True
+            # compute the absolute difference between the current frame and first frame
+            frameDelta = cv2.absdiff(firstFrame, gray)
+            thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
+            # dilate the thresholded image to fill in holes, then find contours on thresholded image
+            thresh = cv2.dilate(thresh, None, iterations=2)
+            contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours = imutils.grab_contours(contours)
 
+            # loop over the contours
+            for contour in contours:
+                # if the contour is too small, ignore it
+                if cv2.contourArea(contour) < args["min_area"]:
+                    continue
+
+                if not light_on:
+                    print(f"{datetime.datetime.now()}: Motion detected")
+                    switches.switch_on(device)
+                    light_on = True
